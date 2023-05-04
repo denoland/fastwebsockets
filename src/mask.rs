da2b97ec292a02ae25d7c319b4f3cd6f38a5a189
Divy Srivastava <dj.srivastava23@gmail.com>
2023-05-04 17:33:13 +0530

    avx sse4.3

--- src/mask.rs
+++ src/mask.rs
@@ -23,7 +23,27 @@ fn unmask_easy(payload: &mut [u8], mask: [u8; 4]) {
 #[cfg(all(target_arch = "x86_64", feature = "simd"))]
 #[inline]
 pub fn unmask_avx2(payload: &mut [u8], mask: [u8; 4]) {
-  unsafe {
+  use core::mem;
+  use std::sync::atomic::AtomicPtr;
+  use std::sync::atomic::Ordering;
+
+  type FnRaw = *mut ();
+  type FnImpl = unsafe fn(&mut [u8], [u8; 4]);
+
+  unsafe fn get_impl(input: &[u8]) {
+    let fun = if std::is_x86_feature_detected!("avx2") {
+      avx2
+    } else if std::is_x86_feature_detected!("sse4.2") {
+      sse42
+    } else {
+      unmask_easy
+    };
+    FN.store(fun as FnRaw, Ordering::Relaxed);
+    (fun)(input)
+  }
+
+  #[inline]
+  unsafe fn sse42(payload: &mut [u8], mask: [u8; 4]) {
     use std::arch::x86_64::*;
 
     let mask_m = _mm_loadu_si128(mask.as_ptr() as *const _);
@@ -34,8 +54,34 @@ pub fn unmask_avx2(payload: &mut [u8], mask: [u8; 4]) {
       _mm_storeu_si128(payload.as_mut_ptr().add(i) as *mut _, data);
       i += 16;
     }
+
     unmask_easy(&mut payload[i..], mask);
   }
+
+  #[inline]
+  unsafe fn avx2(payload: &mut [u8], mask: [u8; 4]) {
+    use std::arch::x86_64::*;
+
+    let mask_m = _mm256_loadu_si256(mask.as_ptr() as *const _);
+    let mut i = 0;
+    while i + 32 <= payload.len() {
+      let mut data = _mm256_loadu_si256(payload.as_ptr().add(i) as *const _);
+      data = _mm256_xor_si256(data, mask_m);
+      _mm256_storeu_si256(payload.as_mut_ptr().add(i) as *mut _, data);
+      i += 32;
+    }
+
+    sse42(&mut payload[i..], mask);
+  }
+
+  static FN: AtomicPtr<()> = AtomicPtr::new(get_impl as FnRaw);
+
+  if payload.len() < 16 {
+    return unmask_easy(payload, mask);
+  }
+
+  let fun = FN.load(Ordering::Relaxed);
+  unsafe { mem::transmute::<FnRaw, FnImpl>(fun)(payload, mask) }
 }
 
 /// Unmask a payload using the given 4-byte mask.
