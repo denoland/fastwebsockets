88a15b5a55a06d97373bc619a3db9292108e60d9
Matt Mastracci <matthew@mastracci.com>
2023-10-30 10:51:23 -0600

    feat: split WebSocket (#48)

---
+++ examples/echo_server_split.rs
@@ -0,0 +1,84 @@
+// Copyright 2023 Divy Srivastava <dj.srivastava23@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use fastwebsockets::upgrade;
+use fastwebsockets::FragmentCollectorRead;
+use fastwebsockets::OpCode;
+use fastwebsockets::WebSocketError;
+use hyper::server::conn::Http;
+use hyper::service::service_fn;
+use hyper::Body;
+use hyper::Request;
+use hyper::Response;
+use tokio::net::TcpListener;
+
+async fn handle_client(fut: upgrade::UpgradeFut) -> Result<(), WebSocketError> {
+  let ws = fut.await?;
+  let (rx, mut tx) = ws.split(|ws| tokio::io::split(ws));
+  let mut rx = FragmentCollectorRead::new(rx);
+  loop {
+    // Empty send_fn is fine because the benchmark does not create obligated writes.
+    let frame = rx
+      .read_frame::<_, WebSocketError>(&mut move |_| async {
+        unreachable!();
+      })
+      .await?;
+    match frame.opcode {
+      OpCode::Close => break,
+      OpCode::Text | OpCode::Binary => {
+        tx.write_frame(frame).await?;
+      }
+      _ => {}
+    }
+  }
+
+  Ok(())
+}
+async fn server_upgrade(
+  mut req: Request<Body>,
+) -> Result<Response<Body>, WebSocketError> {
+  let (response, fut) = upgrade::upgrade(&mut req)?;
+
+  tokio::task::spawn(async move {
+    if let Err(e) = tokio::task::unconstrained(handle_client(fut)).await {
+      eprintln!("Error in websocket connection: {}", e);
+    }
+  });
+
+  Ok(response)
+}
+
+fn main() -> Result<(), WebSocketError> {
+  let rt = tokio::runtime::Builder::new_current_thread()
+    .enable_io()
+    .build()
+    .unwrap();
+
+  rt.block_on(async move {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("Server started, listening on {}", "127.0.0.1:8080");
+    loop {
+      let (stream, _) = listener.accept().await?;
+      println!("Client connected");
+      tokio::spawn(async move {
+        let conn_fut = Http::new()
+          .serve_connection(stream, service_fn(server_upgrade))
+          .with_upgrades();
+        if let Err(e) = conn_fut.await {
+          println!("An error occurred: {:?}", e);
+        }
+      });
+    }
+  })
+}
---
+++ tests/split.rs
@@ -0,0 +1,157 @@
+// Copyright 2023 Divy Srivastava <dj.srivastava23@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use anyhow::Result;
+use fastwebsockets::upgrade;
+use fastwebsockets::Frame;
+use fastwebsockets::OpCode;
+use hyper::server::conn::Http;
+use hyper::service::service_fn;
+use hyper::Body;
+use hyper::Request;
+use hyper::Response;
+use tokio::net::TcpListener;
+
+use fastwebsockets::handshake;
+use fastwebsockets::WebSocketRead;
+use fastwebsockets::WebSocketWrite;
+use hyper::header::CONNECTION;
+use hyper::header::UPGRADE;
+use hyper::upgrade::Upgraded;
+use tokio::sync::Mutex;
+
+use std::future::Future;
+use std::rc::Rc;
+
+use tokio::net::TcpStream;
+
+const N_CLIENTS: usize = 20;
+
+async fn handle_client(
+  client_id: usize,
+  fut: upgrade::UpgradeFut,
+) -> Result<()> {
+  let mut ws = fut.await?;
+  ws.set_writev(false);
+  let mut ws = fastwebsockets::FragmentCollector::new(ws);
+
+  ws.write_frame(Frame::binary(client_id.to_ne_bytes().as_ref().into()))
+    .await
+    .unwrap();
+
+  Ok(())
+}
+
+async fn server_upgrade(mut req: Request<Body>) -> Result<Response<Body>> {
+  let (response, fut) = upgrade::upgrade(&mut req)?;
+
+  let client_id: usize = req
+    .headers()
+    .get("CLIENT-ID")
+    .unwrap()
+    .to_str()
+    .unwrap()
+    .parse()
+    .unwrap();
+  tokio::spawn(async move {
+    handle_client(client_id, fut).await.unwrap();
+  });
+
+  Ok(response)
+}
+
+async fn connect(
+  client_id: usize,
+) -> Result<(
+  WebSocketRead<tokio::io::ReadHalf<Upgraded>>,
+  WebSocketWrite<tokio::io::WriteHalf<Upgraded>>,
+)> {
+  let stream = TcpStream::connect("localhost:8080").await?;
+
+  let req = Request::builder()
+    .method("GET")
+    .uri("http://localhost:8080/")
+    .header("Host", "localhost:8080")
+    .header(UPGRADE, "websocket")
+    .header(CONNECTION, "upgrade")
+    .header("CLIENT-ID", &format!("{}", client_id))
+    .header(
+      "Sec-WebSocket-Key",
+      fastwebsockets::handshake::generate_key(),
+    )
+    .header("Sec-WebSocket-Version", "13")
+    .body(Body::empty())?;
+
+  let (ws, _) = handshake::client(&SpawnExecutor, req, stream).await?;
+  Ok(ws.split(|s| tokio::io::split(s)))
+}
+
+async fn start_client(client_id: usize) -> Result<()> {
+  let (mut r, w) = connect(client_id).await.unwrap();
+  let w = Rc::new(Mutex::new(w));
+  let frame = r
+    .read_frame(&mut move |frame| {
+      let w = w.clone();
+      async move { w.lock().await.write_frame(frame).await }
+    })
+    .await?;
+  match frame.opcode {
+    OpCode::Close => {}
+    OpCode::Binary => {
+      let n = usize::from_ne_bytes(frame.payload[..].try_into().unwrap());
+      assert_eq!(n, client_id);
+    }
+    _ => {
+      panic!("Unexpected");
+    }
+  }
+  Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test() -> Result<()> {
+  let listener = TcpListener::bind("127.0.0.1:8080").await?;
+  println!("Server started, listening on {}", "127.0.0.1:8080");
+  tokio::spawn(async move {
+    loop {
+      let (stream, _) = listener.accept().await.unwrap();
+      tokio::spawn(async move {
+        let conn_fut = Http::new()
+          .serve_connection(stream, service_fn(server_upgrade))
+          .with_upgrades();
+        conn_fut.await.unwrap();
+      });
+    }
+  });
+  let mut tasks = Vec::with_capacity(N_CLIENTS);
+  for client in 0..N_CLIENTS {
+    tasks.push(start_client(client));
+  }
+  for handle in tasks {
+    handle.await.unwrap();
+  }
+  Ok(())
+}
+
+struct SpawnExecutor;
+
+impl<Fut> hyper::rt::Executor<Fut> for SpawnExecutor
+where
+  Fut: Future + Send + 'static,
+  Fut::Output: Send + 'static,
+{
+  fn execute(&self, fut: Fut) {
+    tokio::task::spawn(fut);
+  }
+}
