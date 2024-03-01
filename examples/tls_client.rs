41c17f34470f7c7b60a9de4ab3372d02fbecdc1f
parastrom <adewoletomisin78@gmail.com>
2024-03-01 21:57:48 +0000

    feat: tls_client ex

--- examples/tls_client.rs
+++ examples/tls_client.rs
@@ -11,11 +11,28 @@ use hyper::header::CONNECTION;
 use hyper::header::UPGRADE;
 use hyper::upgrade::Upgraded;
 use hyper::Request;
-use hyper_util::rt::TokioIo;
+
+#[cfg(feature = "futures")]
+use async_std::net::TcpStream;
+#[cfg(feature = "futures")]
+use fastwebsockets::FuturesIo as IoWrapper;
+#[cfg(feature = "futures")]
+use futures_rustls::{
+  rustls::{
+    pki_types::{self, TrustAnchor},
+    ClientConfig, RootCertStore,
+  },
+  TlsConnector,
+};
+#[cfg(not(feature = "futures"))]
+use hyper_util::rt::TokioIo as IoWrapper;
+#[cfg(not(feature = "futures"))]
 use tokio::net::TcpStream;
-use tokio_rustls::rustls::ClientConfig;
-use tokio_rustls::rustls::OwnedTrustAnchor;
-use tokio_rustls::TlsConnector;
+#[cfg(not(feature = "futures"))]
+use tokio_rustls::{
+  rustls::{ClientConfig, OwnedTrustAnchor, RootCertStore},
+  TlsConnector,
+};
 
 struct SpawnExecutor;
 
@@ -25,41 +42,69 @@ where
   Fut::Output: Send + 'static,
 {
   fn execute(&self, fut: Fut) {
+    #[cfg(not(feature = "futures"))]
     tokio::task::spawn(fut);
+
+    #[cfg(feature = "futures")]
+    async_std::task::spawn(fut);
   }
 }
 
 fn tls_connector() -> Result<TlsConnector> {
-  let mut root_store = tokio_rustls::rustls::RootCertStore::empty();
-
-  root_store.add_server_trust_anchors(
-    webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+  let mut root_store = RootCertStore::empty();
+  #[cfg(not(feature = "futures"))]
+  root_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(
+    |ta| {
       OwnedTrustAnchor::from_subject_spki_name_constraints(
         ta.subject,
         ta.spki,
         ta.name_constraints,
       )
-    }),
-  );
+    },
+  ));
 
+  #[cfg(feature = "futures")]
+  root_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
+    let ta = ta.to_owned();
+    TrustAnchor {
+      subject: ta.subject.into(),
+      subject_public_key_info: ta.spki.into(),
+      name_constraints: ta.name_constraints.map(Into::into),
+    }
+  }));
+
+  #[cfg(not(feature = "futures"))]
   let config = ClientConfig::builder()
     .with_safe_defaults()
     .with_root_certificates(root_store)
     .with_no_client_auth();
-
+  #[cfg(feature = "futures")]
+  let config = ClientConfig::builder()
+    .with_root_certificates(root_store)
+    .with_no_client_auth();
   Ok(TlsConnector::from(Arc::new(config)))
 }
 
-async fn connect(domain: &str) -> Result<FragmentCollector<TokioIo<Upgraded>>> {
+async fn connect(
+  domain: &str,
+) -> Result<FragmentCollector<IoWrapper<Upgraded>>> {
   let mut addr = String::from(domain);
   addr.push_str(":9443"); // Port number for binance stream
 
   let tcp_stream = TcpStream::connect(&addr).await?;
   let tls_connector = tls_connector().unwrap();
+  #[cfg(not(feature = "futures"))]
   let domain =
     tokio_rustls::rustls::ServerName::try_from(domain).map_err(|_| {
       std::io::Error::new(std::io::ErrorKind::InvalidInput, "invalid dnsname")
     })?;
+
+  #[cfg(feature = "futures")]
+  let domain = pki_types::ServerName::try_from(domain)
+    .map_err(|_| {
+      std::io::Error::new(std::io::ErrorKind::InvalidInput, "invalid dnsname")
+    })?
+    .to_owned();
 
   let tls_stream = tls_connector.connect(domain, tcp_stream).await?;
 
@@ -81,7 +126,19 @@ async fn connect(domain: &str) -> Result<FragmentCollector<TokioIo<Upgraded>>> {
   Ok(FragmentCollector::new(ws))
 }
 
-#[tokio::main(flavor = "current_thread")]
+macro_rules! runtime_main {
+    ($($body:tt)*) => {
+        #[cfg(feature = "futures")]
+        #[async_std::main]
+        $($body)*
+
+        #[cfg(not(feature = "futures"))]
+        #[tokio::main]
+        $($body)*
+    };
+}
+
+runtime_main! {
 async fn main() -> Result<()> {
   let domain = "data-stream.binance.com";
   let mut ws = connect(domain).await?;
@@ -110,4 +167,5 @@ async fn main() -> Result<()> {
     }
   }
   Ok(())
+}
 }
